3d763f6e0794553f6f4677b271e2115a8161b818
pablomd <a23pablomd@iessanclemente.net>
2024-11-25 07:29:32 +0100

    Creacion fragmentos. Anhadido sistema de navegación no lineal

---
+++ UF1_Proyecto/app/src/main/java/com/example/uf1_proyecto/MisPeliculasFragment.kt
@@ -0,0 +1,22 @@
+package com.example.uf1_proyecto
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+
+class MisPeliculasFragment : Fragment() {
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_mis_peliculas, container, false)
+    }
+
+
+}
