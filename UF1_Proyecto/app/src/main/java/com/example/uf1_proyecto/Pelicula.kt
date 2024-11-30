deedaa0137ddf783ca64ce37d58b63c94dba80bf
pablomd <a23pablomd@iessanclemente.net>
2024-11-30 18:32:19 +0100

    Conexión a TMDB Api.

--- UF1_Proyecto/app/src/main/java/com/example/uf1_proyecto/Pelicula.kt
+++
@@ -1,9 +0,0 @@
-package com.example.uf1_proyecto
-
-data class Pelicula(
-    val titulo: String,
-    val director: String,
-    val anho: String,
-    val pais: String,
-    val sinopsis: String
-)
